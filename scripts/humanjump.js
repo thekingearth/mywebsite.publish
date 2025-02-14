554201629ea243a6f7474ae2e84748db10c66bc0
kinbasari <kinbasari@gmail.com>
2025-02-14 12:05:10 +0700

    humanjump.js

--- scripts/humanjump.js
+++ scripts/humanjump.js
@@ -65,6 +65,27 @@
     }, 1000);
   });
 
+// Audio players hero jump
+const startLoopButton = document.getElementById('herojump');
+    const audioLoop = document.getElementById('audioLoop');
+
+    startLoopButton.addEventListener('click', () => {
+      audioLoop.play(); // Memulai audio looping
+    });
+
+    // Mengatur event listener untuk semua tombol dengan kelas "playButton"
+    const buttons = document.querySelectorAll('.jump');
+    const audioPlayer = document.getElementById('audioPlayer');
+
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        const audioSrc = button.getAttribute('data-audio'); // Mendapatkan sumber audio
+        audioPlayer.src = audioSrc; // Mengatur sumber audio
+        audioPlayer.currentTime = 0; // Memastikan audio dimulai dari awal
+        audioPlayer.play(); // Memutar audio
+      });
+    });
+    
 
 // Pop up game over
 function showGameOverPopup() {
