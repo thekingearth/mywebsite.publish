11f2dd47a36ed6e54787e8bcbdbf00b51f54d72c
kinbasari <kinbasari@gmail.com>
2025-02-18 07:59:03 +0700

    bookitems.js

--- scripts/bookitems.js
+++ scripts/bookitems.js
@@ -67,3 +67,14 @@ const rupiah = (number) => {
     minimumFractionDigits: 0,
   }).format(number);
 };
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7QHXL65D77"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7QHXL65D77');
+</script>
