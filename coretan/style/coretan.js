cc9f077b01c6998210c57ba0922aebc4418187a6
kinbasari <kinbasari@gmail.com>
2025-02-19 06:44:35 +0700

    coretan.js

---
+++ coretan/style/coretan.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const folderPath = '../'; // Ganti dengan path folder artikel
+  const articles = [
+    { file: 'cara-santai-bikin-tulisan-yang-asik-dibaca.html', title: 'Cara Santai Bikin Tulisan yang Asik Dibaca' },
+    { file: 'dari-kata-jadi-karya-serunya-nulis.html', title: 'Dari Kata Jadi Karya, Serunya Nulis' },
+    { file: 'ketahui-bakat-terpendammu.html', title: 'Ketahui Bakat Terpendammu' },
+    { file: 'kisah-inspiratif-untuk-semangat.hidup.html', title: 'Kisah Inspiratif yang Bikin Hidup Lebih Bersemangat' },
+    { file: 'menulis-itu-healing-tumpahin-pikiran-legain-hati.html', title: 'Menulis Itu Healing! Tumpahin Pikiran, Legain Hati' },
+    { file: 'stop-minder-langkah-simpel-buat-up.html', title: 'Stop Minder! Langkah simpel buat Upgrade Diri' }
+    ];
+
+  const listContainer = document.getElementById("related-artikel");
+  // Acak array artikel dengan metode sort dan Math.random
+  const shuffled = articles.sort(() => 0.5 - Math.random());
+  
+  // Ambil 5 artikel dari hasil pengacakan
+  const selectedArticles = shuffled.slice(0, 5);
+
+  // Render artikel terpilih ke dalam list
+  selectedArticles.forEach(article => {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = folderPath + article.file;
+    link.textContent = article.title;
+    listItem.appendChild(link);
+    listContainer.appendChild(listItem);
+   });
+  });
