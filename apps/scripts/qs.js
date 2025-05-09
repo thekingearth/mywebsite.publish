5e90ea3e40353be27b8b5ddcae280c8082c7fa70
kinbasari <kinbasari@gmail.com>
2025-05-09 15:50:29 +0700

    Create qs.js

---
+++ apps/scripts/qs.js
@@ -0,0 +1,39 @@
+
+let scrollSpeed = 1;
+  let interval = null;
+  let isScrolling = false;
+
+  function toggleScroll() {
+    if (isScrolling) {
+      stopScroll();
+    } else {
+      startScroll();
+    }
+  }
+
+  function startScroll() {
+    interval = setInterval(() => {
+      window.scrollBy(0, scrollSpeed);
+    }, 50);
+    isScrolling = true;
+    document.getElementById("scrollBtnText").textContent = "❚❚";
+  }
+
+  function stopScroll() {
+    clearInterval(interval);
+    isScrolling = false;
+    document.getElementById("scrollBtnText").textContent = "▶";
+  }
+function increaseSpeed() {
+    if (scrollSpeed < 20) scrollSpeed += 1;
+    updateSpeedDisplay();
+  }
+
+  function decreaseSpeed() {
+    if (scrollSpeed > 1) scrollSpeed -= 1;
+    updateSpeedDisplay();
+  }
+
+  function updateSpeedDisplay() {
+    document.getElementById("speedDisplay").textContent = "" + scrollSpeed;
+  }
