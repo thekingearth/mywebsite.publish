1a0a83ca237c8054f960fb1d2ec902496e4bd864
kingearth <kinbasari@gmail.com>
2025-05-16 15:32:22 +0700

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,9 +2,27 @@ feather.replace();
 fetch('../menu/navbar.html')
 .then(res => res.text())
 .then(html => {
-  document.getElementById('navbar-container').innerHTML = html;
-  feather.replace();
-  const script = document.createElement('script');
-  script.src = 'myscript.js';
-  document.body.appendChild(script);
+  const navbarNav = document.querySelector('.navbar-nav');
+      const shoppingCart = document.querySelector('.shopping-cart');
+      const hm = document.querySelector('#hamburger-menu');
+      const sc = document.querySelector('#shopping-cart-button');
+
+      hm.onclick = () => {
+        navbarNav.classList.toggle('active');
+      };
+
+      sc.onclick = (e) => {
+        shoppingCart.classList.toggle('active');
+        e.preventDefault();
+      };
+
+      document.addEventListener('click', function(e) {
+        if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+          navbarNav.classList.remove('active');
+        }
+
+        if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+          shoppingCart.classList.remove('active');
+        }
+      });
 });
