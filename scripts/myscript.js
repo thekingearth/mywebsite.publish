7c250dc27a8a894e0060648e0ffffb49688c2c8a
kingearth <kinbasari@gmail.com>
2025-06-04 16:33:40 +0700

    Update myscript.js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,55 +1,28 @@
-// Toggle class active hamburger menu
-const navbarNav = document.querySelector('.navbar-nav');
-// Saat hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarNav = document.querySelector('.navbar-nav');
+  const hamburgerMenu = document.querySelector('#hamburger-menu');
+  const shoppingCart = document.querySelector('.shopping-cart');
+  const shoppingCartButton = document.querySelector('#shopping-cart-button');
 
+  // Toggle class active hamburger menu
+  hamburgerMenu.onclick = () => {
+    navbarNav.classList.toggle('active');
+  };
 
-// Toggle class active shopping cart
-const shoppingCart = document.querySelector('.shopping-cart');
+  // Toggle class active shopping cart
+  shoppingCartButton.onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+  };
 
-document.querySelector('#shopping-cart-button').onclick = (e) => {
-  shoppingCart.classList.toggle('active');
-  e.preventDefault();
-};
-
-
-// klik luar sidbar hilang nav
-
-const hm = document.querySelector('#hamburger-menu');
-const sc = document.querySelector('#shopping-cart-button')
-
-document.addEventListener('click', function(e) {
-  if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
-  }
-  
-  
-  if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove('active');
-  }
-});
-
-// Form validation
-const checkoutButton = document.querySelector('.checkout-button');
-checkoutButton.disabled = true;
-
-const form = document.querySelector('#checkoutForm');
-
-form.addEventListener('keyup', function() {
-  for(let i = 0; i < form.elements.length; i++) {
-    if(form.elements[i].value.length !== 0) {
-      checkoutButton.classList.remove('disabled');
-      checkoutButton.classList.add('disabled');
-    } else {
-      return false;
+  // Klik luar untuk sembunyikan nav atau cart
+  document.addEventListener('click', function (e) {
+    if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+      navbarNav.classList.remove('active');
     }
-  }
-  checkoutButton.disabled = false;
-  checkoutButton.classList.remove('disabled');
+
+    if (!shoppingCartButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+      shoppingCart.classList.remove('active');
+    }
+  });
 });
-
-
-  
-
