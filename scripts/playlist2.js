e0166e7a8b7f25fbf4bb3798c50d508fd75eabfb
kingearth <kinbasari@gmail.com>
2025-06-17 20:17:22 +0700

    Update playlist2.js

--- scripts/playlist2.js
+++ scripts/playlist2.js
@@ -1,31 +1,33 @@
 export const playlist = [
   "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
-  "https://airinerra.github.io/helloword/playlist/bole.mp3",
-  "https://airinerra.github.io/helloword/playlist/dolls.mp3",
-  "https://airinerra.github.io/helloword/playlist/ambahky.mp3",
-  "https://airinerra.github.io/helloword/playlist/entatani.mp3",
-  "https://airinerra.github.io/helloword/playlist/goryachaya.mp3",
-  "https://airinerra.github.io/helloword/playlist/hafdal.mp3",
-  "https://airinerra.github.io/helloword/playlist/slishkim.mp3",
-  "https://airinerra.github.io/helloword/playlist/nifsi.mp3",
-  "https://airinerra.github.io/helloword/playlist/siahbo.mp3",
-  "https://airinerra.github.io/helloword/playlist/touta.mp3",
-  "https://airinerra.github.io/helloword/playlist/igrushka.mp3",
-  "https://airinerra.github.io/helloword/playlist/sentyabra.mp3"
+  "https://airinerra.github.io/helloword/zaynep/ara.mp3",
+  "https://airinerra.github.io/helloword/zaynep/bulid.mp3",
+  "https://airinerra.github.io/helloword/zaynep/calm.mp3",
+  "https://airinerra.github.io/helloword/zaynep/come.mp3",
+  "https://airinerra.github.io/helloword/zaynep/egmady.mp3",
+  "https://airinerra.github.io/helloword/zaynep/eihyalli.mp3",
+  "https://airinerra.github.io/helloword/zaynep/ezzay.mp3",
+  "https://airinerra.github.io/helloword/zaynep/atsmu.mp3",
+  "https://airinerra.github.io/helloword/zaynep/aintl.mp3",
+  "https://airinerra.github.io/helloword/zaynep/dushi.mp3",
+  "https://airinerra.github.io/helloword/zaynep/pocered.mp3",
+  "https://airinerra.github.io/helloword/zaynep/zamelo.mp3"
   ];
 
 export const titles = [
   "Opening Track",
-  "Bolelut",
-  "Dolls",
-  "Eam Behky Me Halle",
-  "Enta Teni",
-  "Goryacheya Gremucheya",
-  "Hafdel",
-  "My Sleshkim Raznyye",
-  "Nefsi Aoulhalek",
-  "Sheyohavu Ote Kecha",
-  "Teuta",
-  "Ya Ne Igreshka",
-  "Ze Sentyebreya"
+  "Ara",
+  "Bulid a bitch",
+  "Calm down",
+  "Come get it",
+  "Egmady",
+  "Eih yalli byohsal",
+  "Ezzay ansak",
+  "Hozer al atzmu",
+  "Intelecto'ars",
+  "Khvora na lyubov",
+  "Ne dushi",
+  "Posered nochi",
+  "Zamelo"
   ];
+
