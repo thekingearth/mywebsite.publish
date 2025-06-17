d73de02bf597f2bda619bc2777775d1d416b8f20
kingearth <kinbasari@gmail.com>
2025-06-17 20:18:22 +0700

    Create playlist3.js

---
+++ scripts/playlist3.js
@@ -0,0 +1,31 @@
+export const playlist = [
+  "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
+  "https://airinerra.github.io/helloword/poarch/sifar.mp3",
+  "https://airinerra.github.io/helloword/poarch/vimim.mp3",
+  "https://airinerra.github.io/helloword/poarch/lasmarany.mp3",
+  "https://airinerra.github.io/helloword/poarch/inferno.mp3",
+  "https://airinerra.github.io/helloword/poarch/kapkan.mp3",
+  "https://airinerra.github.io/helloword/poarch/love.mp3",
+  "https://airinerra.github.io/helloword/poarch/matolch.mp3",
+  "https://airinerra.github.io/helloword/poarch/ananeya.mp3",
+  "https://airinerra.github.io/helloword/poarch/solo.mp3",
+  "https://airinerra.github.io/helloword/poarch/takii.mp3",
+  "https://airinerra.github.io/helloword/poarch/pidu.mp3"
+  ];
+
+export const titles = [
+  "Opening Track",
+  "Rek shiyishe'er",
+  "Bimim hahem",
+  "El asmarany",
+  "Inferno",
+  "Iyyam el citty",
+  "Kapkan",
+  "Love you like a love song",
+  "Matolch li had",
+  "Mosh ananeya",
+  "Solo",
+  "Takyy yak usi",
+  "Ya pidu"
+  ];
+
