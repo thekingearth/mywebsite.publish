fa071ad3cdd8ef741ab981502a2d303b674fc568
kingearth <kinbasari@gmail.com>
2025-06-17 20:37:54 +0700

    Update playlist3.js

--- scripts/playlist3.js
+++ scripts/playlist3.js
@@ -4,6 +4,7 @@ export const playlist = [
   "https://airinerra.github.io/helloword/poarch/vimim.mp3",
   "https://airinerra.github.io/helloword/poarch/lasmarany.mp3",
   "https://airinerra.github.io/helloword/poarch/inferno.mp3",
+  "https://airinerra.github.io/helloword/poarch/iyyam.mp3",
   "https://airinerra.github.io/helloword/poarch/kapkan.mp3",
   "https://airinerra.github.io/helloword/poarch/love.mp3",
   "https://airinerra.github.io/helloword/poarch/matolch.mp3",
