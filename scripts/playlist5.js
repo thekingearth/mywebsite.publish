087134dac00152f56979a26f010255babcc45962
kingearth <kinbasari@gmail.com>
2025-06-17 20:20:30 +0700

    Create playlist5.js

---
+++ scripts/playlist5.js
@@ -0,0 +1,21 @@
+export const playlist = [
+  "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
+  "https://airinerra.github.io/helloword/addinu/addinu.mp3",
+  "https://airinerra.github.io/helloword/addinu/alamate.mp3",
+  "https://airinerra.github.io/helloword/addinu/cokot.mp3",
+  "https://airinerra.github.io/helloword/addinu/itaneng.mp3",
+  "https://airinerra.github.io/helloword/addinu/sluku.mp3",
+  "https://airinerra.github.io/helloword/addinu/wali.mp3"
+  ];
+
+export const titles = [
+  "Opening Track",
+  "Addinu Lana",
+  "Alamate anak soleh",
+  "Cokot boyo",
+  "Itaneng tenri bolo",
+  "Sluku sluku batok",
+  "Wali songo"
+  ];
+
+
