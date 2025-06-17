99ada07e084974c60f5cb05e8dae0ec66548f149
kingearth <kinbasari@gmail.com>
2025-06-17 20:19:30 +0700

    Update playlist4.js

--- scripts/playlist4.js
+++ scripts/playlist4.js
@@ -1,1 +1,35 @@
+export const playlist = [
+  "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
+  "https://airinerra.github.io/helloword/dilema/harem.mp3",
+  "https://airinerra.github.io/helloword/dilema/ahavah.mp3",
+  "https://airinerra.github.io/helloword/dilema/mehitch.mp3",
+  "https://airinerra.github.io/helloword/dilema/dilemma.mp3",
+  "https://airinerra.github.io/helloword/dilema/nadiny.mp3",
+  "https://airinerra.github.io/helloword/dilema/navzayemno.mp3",
+  "https://airinerra.github.io/helloword/dilema/psikhushka.mp3",
+  "https://airinerra.github.io/helloword/dilema/shawn.mp3",
+  "https://airinerra.github.io/helloword/dilema/elbanat.mp3",
+  "https://airinerra.github.io/helloword/dilema/unuturum.mp3",
+  "https://airinerra.github.io/helloword/dilema/vilain.mp3",
+  "https://airinerra.github.io/helloword/dilema/toboyu.mp3",
+  "https://airinerra.github.io/helloword/dilema/yarabb.mp3"
+  ];
+
+export const titles = [
+  "Opening Track",
+  "Cha'aram",
+  "Ha'ahava shlanu",
+  "Meh itch aba",
+  "My dilemma",
+  "Nadini",
+  "Nevzayemno",
+  "Psikhushka",
+  "Shawn mendes",
+  "Set el banat",
+  "Unuturum",
+  "Villain",
+  "Z toboyu",
+  "Ya rabb"
+  ];
+
 
