4f91b295b638fcc32000c18a6c82e4056ef8201b
kingearth <kinbasari@gmail.com>
2025-06-18 09:45:17 +0700

    Update and rename playlist3.js to playlist3.js

--- songs/playlist3.js
+++ songs/playlist3.js
@@ -1,14 +1,14 @@
 export const playlist = [
   "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
-  "https://airinerra.github.io/helloword/poarch/sifar.mp3",
-  "https://airinerra.github.io/helloword/poarch/vimim.mp3",
-  "https://airinerra.github.io/helloword/poarch/lasmarany.mp3",
-  "https://airinerra.github.io/helloword/poarch/inferno.mp3",
   "https://airinerra.github.io/helloword/poarch/iyyam.mp3",
+  "https://airinerra.github.io/helloword/poarch/vimim.mp3",
+  "https://airinerra.github.io/helloword/poarch/inferno.mp3",
   "https://airinerra.github.io/helloword/poarch/kapkan.mp3",
+  "https://airinerra.github.io/helloword/poarch/lasmarany.mp3",
   "https://airinerra.github.io/helloword/poarch/love.mp3",
   "https://airinerra.github.io/helloword/poarch/matolch.mp3",
   "https://airinerra.github.io/helloword/poarch/ananeya.mp3",
+  "https://airinerra.github.io/helloword/poarch/sifar.mp3",
   "https://airinerra.github.io/helloword/poarch/solo.mp3",
   "https://airinerra.github.io/helloword/poarch/takii.mp3",
   "https://airinerra.github.io/helloword/poarch/pidu.mp3"
@@ -16,17 +16,17 @@ export const playlist = [
 
 export const titles = [
   "Opening Track",
-  "Rek shiyishe'er",
-  "Bimim hahem",
-  "El asmarany",
+  "Ayyam El Cetty",
+  "Bemim Hahem",
   "Inferno",
-  "Iyyam el citty",
   "Kapkan",
-  "Love you like a love song",
-  "Matolch li had",
-  "Mosh ananeya",
+  "Lasmarany",
+  "Love You Like A Love Song",
+  "Metolch Le Had",
+  "Mesh Enaneya",
+  "Rek Shiyishe'er",
   "Solo",
-  "Takyy yak usi",
-  "Ya pidu"
+  "Takyy Yak Usi",
+  "Ya Pedyu"
   ];
 
