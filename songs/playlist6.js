f63d8da68581ac72f16885fec7710341bcb3dfe1
kingearth <kinbasari@gmail.com>
2025-06-18 09:51:55 +0700

    Create playlist6.js

---
+++ songs/playlist6.js
@@ -0,0 +1,42 @@
+export const playlist = [
+  "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
+  "https://airinerra.github.io/helloword/playlist/bole.mp3",
+  "https://airinerra.github.io/helloword/playlist/dolls.mp3",
+  "https://airinerra.github.io/helloword/playlist/ambahky.mp3",
+  "https://airinerra.github.io/helloword/playlist/entatani.mp3",
+  "https://airinerra.github.io/helloword/playlist/goryachaya.mp3",
+  "https://airinerra.github.io/helloword/playlist/hafdal.mp3",
+  "https://airinerra.github.io/helloword/playlist/hellokitty.mp3",
+  "https://airinerra.github.io/helloword/playlist/im.mp3",
+  "https://airinerra.github.io/helloword/playlist/living.mp3",
+  "https://airinerra.github.io/helloword/playlist/mahasiv.mp3",
+  "https://airinerra.github.io/helloword/playlist/muhur.mp3",
+  "https://airinerra.github.io/helloword/playlist/slishkim.mp3",
+  "https://airinerra.github.io/helloword/playlist/nifsi.mp3",
+  "https://airinerra.github.io/helloword/playlist/siahbo.mp3",
+  "https://airinerra.github.io/helloword/playlist/touta.mp3",
+  "https://airinerra.github.io/helloword/playlist/igrushka.mp3",
+  "https://airinerra.github.io/helloword/playlist/sentyabra.mp3"
+  ];
+
+export const titles = [
+  "Opening Track",
+  "Bolelut",
+  "Dolls",
+  "Eam Behky Me Halle",
+  "Enta Teni",
+  "Goryacheya Gremucheya",
+  "Hafdel",
+  "Hello Kitti",
+  "Jerusalem",
+  "Living Hell",
+  "Ma Ashiv Lecha",
+  "Mehur",
+  "My Sleshkim Raznyye",
+  "Nefsi Aoulhalek",
+  "Sheyohavu Ote Kecha",
+  "Teuta",
+  "Ya Ne Igreshka",
+  "Ze Sentyebreya"
+  ];
+
