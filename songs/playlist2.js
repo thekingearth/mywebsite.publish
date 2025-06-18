88b7792b9c3b9e95fd3b2bd6a8f78759d3e531da
kingearth <kinbasari@gmail.com>
2025-06-18 10:15:13 +0700

    Update playlist2.js

--- songs/playlist2.js
+++ songs/playlist2.js
@@ -1,33 +1,33 @@
 export const playlist = [
   "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
+  "https://airinerra.github.io/helloword/zaynep/egmady.mp3",
   "https://airinerra.github.io/helloword/zaynep/ara.mp3",
   "https://airinerra.github.io/helloword/zaynep/buildabt.mp3",
   "https://airinerra.github.io/helloword/zaynep/calm.mp3",
   "https://airinerra.github.io/helloword/zaynep/come.mp3",
-  "https://airinerra.github.io/helloword/zaynep/egmady.mp3",
   "https://airinerra.github.io/helloword/zaynep/eihyalli.mp3",
-  "https://airinerra.github.io/helloword/zaynep/ezzay.mp3",
   "https://airinerra.github.io/helloword/zaynep/atsmu.mp3",
   "https://airinerra.github.io/helloword/zaynep/aintl.mp3",
   "https://airinerra.github.io/helloword/zaynep/khvorana.mp3",
   "https://airinerra.github.io/helloword/zaynep/dushi.mp3",
+  "https://airinerra.github.io/helloword/zaynep/ezzay.mp3",
   "https://airinerra.github.io/helloword/zaynep/pocered.mp3",
   "https://airinerra.github.io/helloword/zaynep/zamelo.mp3"
   ];
 
 export const titles = [
   "Opening Track",
+  "Agmedy",
   "Are",
-  "Build A Bitch",
+  "Build A B*tch",
   "Calm Down",
   "Come Get It",
-  "Egmady",
   "Eih Yealli Byohsel",
-  "Ezzey Ansek",
   "Hozer El Atzmu",
   "Intelecto'ars",
   "Khvora Na Lyubov",
   "Ne Dushi",
+  "Ozzey Ansek",
   "Pusired Noche",
   "Zamilyu"
   ];
