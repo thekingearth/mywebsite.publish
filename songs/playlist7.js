0a69f0d6aa2e1c5279b5088f94ded0b9022b3e81
kingearth <kinbasari@gmail.com>
2025-07-01 13:48:17 +0700

    Create playlist7.js

---
+++ songs/playlist7.js
@@ -0,0 +1,28 @@
+
+export const playlist = [
+  "https://airinerra.github.io/helloword/playlist/baleylot.mp3",
+  "https://airinerra.github.io/helloword/walker/wonderland.mp3",
+  "https://airinerra.github.io/helloword/walker/stereolove.mp3",
+  "https://airinerra.github.io/helloword/walker/unity.mp3",
+  "https://airinerra.github.io/helloword/walker/onmywhy.mp3",
+  "https://airinerra.github.io/helloword/walker/godzilla.mp3",
+  "https://airinerra.github.io/helloword/walker/strongest.mp3",
+  "https://airinerra.github.io/helloword/walker/thedrum.mp3",
+  "https://airinerra.github.io/helloword/walker/lost.mp3",
+  "https://airinerra.github.io/helloword/walker/alone.mp3",
+  "https://airinerra.github.io/helloword/walker/notyou.mp3"
+  ];
+
+export const titles = [
+  "Opening Track",
+  "Ahabet Stri'u",
+  "Artz Haflaot",
+  "Echdot",
+  "El Lema Shli",
+  "Godzilla",
+  "Haki Chzek",
+  "Hatof",
+  "Ibed Shlita",
+  "Levad",
+  "Lo Otach"
+  ];
